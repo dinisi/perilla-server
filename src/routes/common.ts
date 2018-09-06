4d459e91173091ba1b0e0fbcea5fb79671b0e851
ZhangZisu <admin@zhangzisu.cn>
2018-09-06 23:00:59 +0800

    Upload

--- src/routes/common.ts
+++ src/routes/common.ts
@@ -3,8 +3,8 @@ import { ServerError } from "../definitions/errors";
 
 export const validPaginate = (req: Request, res: Response, next: NextFunction) => {
     try {
-        if (!req.query.skip || !parseInt(req.query.skip, 10)) { throw new ServerError("Paginate: invalid skip", 403); }
-        if (!req.query.limit || !parseInt(req.query.limit, 10)) { throw new ServerError("Paginate: invalid limit", 403); }
+        if (!req.query.skip) { throw new ServerError("Paginate: invalid skip", 403); }
+        if (!req.query.limit) { throw new ServerError("Paginate: invalid limit", 403); }
         req.query.skip = parseInt(req.query.skip, 10);
         req.query.limit = parseInt(req.query.limit, 10);
         if (req.query.limit > 256) { throw new ServerError("Limit is too big", 403); }
--- test/index.js
+++ test/index.js
@@ -12,7 +12,7 @@ const request = require('request');
 const rs = require("randomstring");
 
 const username = "Administrator";
-const password = "vhLrgtwDWT";
+const password = "ZlkPRyBzwT";
 const rolename = "Administrators";
 const clientID = "TEST";
 
