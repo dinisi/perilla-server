1758ba030cebb5f7221750a6dfd6d7530055bd3d
ZhangZisu <admin@zhangzisu.cn>
2018-10-04 10:58:34 +0800

    Update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  apps : [{
+  apps: [{
     name: 'LightOnlineJudge',
     script: 'dist/index.js',
     instances: 4,
@@ -12,16 +12,5 @@ module.exports = {
     env_production: {
       NODE_ENV: 'production'
     }
-  }],
-
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  }]
 };
