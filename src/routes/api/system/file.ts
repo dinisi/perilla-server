b838090cdbf3884bd845780605a8d0fb538990d1
ZhangZisu <admin@zhangzisu.cn>
2018-10-18 21:44:12 +0800

    Upload

---
+++ src/routes/api/system/file.ts
@@ -0,0 +1,3 @@
+import { Router } from "express";
+
+export const systemFileRouter = Router();
--- src/routes/api/system/index.ts
+++ src/routes/api/system/index.ts
@@ -1,3 +1,11 @@
 import { Router } from "express";
+import { SystemMap } from "../../../schemas/systemMap";
+import { RESTWarp } from "../wrap";
 
 export const SystemAPIRouter = Router();
+
+SystemAPIRouter.use(RESTWarp((req, res, next) => {
+    if (!req.isAuthenticated()) { throw new Error("Not logged in"); }
+    const map = SystemMap.findOne({ user: req.user });
+    if (!map) { throw new Error("Access denied"); }
+}));
