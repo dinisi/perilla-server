61d7828114a68e83537c39cfdb88811fcdb64183
ZhangZisu <admin@zhangzisu.cn>
2018-12-12 14:51:04 +0800

    v1.4.7

--- src/web/index.ts
+++ src/web/index.ts
@@ -20,7 +20,6 @@ export const startWebService = async () => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
         res.header("Access-Control-Allow-Headers", "Content-Type, x-access-token");
-        res.header("Access-Control-Allow-Credentials", "true");
         if (req.method === "OPTIONS") {
             return res.sendStatus(200);
         } else {
