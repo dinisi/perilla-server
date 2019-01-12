0395309e10843ad68b1abbb8d99411b4de8b58ea
ZhangZisu <admin@zhangzisu.cn>
2019-01-12 12:44:34 +0800

    🛠 Move route hook

--- src/web/routes/api/index.ts
+++ src/web/routes/api/index.ts
@@ -1,7 +1,4 @@
 import { Router } from "express";
-import { verify } from "jsonwebtoken";
-import { config } from "../../../config";
-import { RESTWrap } from "../util";
 import { adminRouter } from "./admin";
 import { ArticleRouter } from "./article";
 import { EntryRouter } from "./entry";
@@ -14,15 +11,6 @@ import { SolutionRouter } from "./solution";
 import { SystemMapRouter } from "./systemmap";
 
 export const APIRouter = Router();
-
-APIRouter.use(RESTWrap(async (req, res, next) => {
-    const token = req.headers["x-access-token"] || (req.body && req.body.access_token) || (req.query && req.query.access_token);
-    if (typeof token === "string" && token) {
-        const decoded = verify(token, config.secret) as any;
-        req.user = decoded._id as string;
-    }
-    return next();
-}));
 
 APIRouter.use("/admin", adminRouter);
 APIRouter.use("/article", ArticleRouter);
--- src/web/routes/index.ts
+++ src/web/routes/index.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { verify } from "jsonwebtoken";
+import { config } from "../../config";
 import { PACKAGE_PATH } from "../../constant";
 import { IRESTResponse } from "../../interfaces/route";
 import { APIRouter } from "./api";
@@ -24,6 +26,15 @@ MainRouter.use((req, res: IRESTResponse, next) => {
 MainRouter.get("/version", RESTWrap(async (req, res) => {
     const { version } = require(PACKAGE_PATH);
     res.RESTSend({ version });
+}));
+
+MainRouter.use(RESTWrap(async (req, res, next) => {
+    const token = req.headers["x-access-token"] || (req.body && req.body.access_token) || (req.query && req.query.access_token);
+    if (typeof token === "string" && token) {
+        const decoded = verify(token, config.secret) as any;
+        req.user = decoded._id as string;
+    }
+    return next();
 }));
 
 MainRouter.use("/api", APIRouter);
